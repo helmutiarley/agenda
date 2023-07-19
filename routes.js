77a68e80723a2b1607a62c473c20b53f1113af4c
Helmut Iarley <78214599+helmutiarley@users.noreply.github.com>
2023-07-19 15:42:05 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const varTest = 'teste';
  
 //Rotas da home
 route.get('/', homeController.index);
